701523e0df11b6fac40483fa39e82124e0ae540f
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-01-12 02:09:01 +0600

    uploaded

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from random import randint
+from flask import *
+from flask_mail import *
+import flask_bcrypt
+import bcrypt
+import pymongo
+
+
+
+db_client = pymongo.MongoClient("mongodb://localhost:27017")
+db = db_client["users_nav"]
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+mail = Mail(app)
+
+app.secret_key = "testing"
+
+from app.routes import index
