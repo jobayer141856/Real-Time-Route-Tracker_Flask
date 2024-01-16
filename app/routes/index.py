11c167bc31f83ca67e4bf01b6dc325db5946389d
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-01-16 21:40:36 +0600

    API Added

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,69 +1,10 @@
-import random
-import requests
+from flask import Flask, render_template
 from app import*
+# Replace 'YOUR_MAP_API_KEY' with your actual map API key
+MAP_API_KEY = 'AIzaSyDu4Yq2QEuR0y_20-eYwu6jJmC_DRwHDaw'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-# @app.route('/calculate', methods=['POST'])
-# def calculate():
-#     origin = request.form['origin']
-#     destination = request.form['destination']
-#     api_key = 'YOUR_GOOGLE_MAPS_API_KEY'
-
-#     # Construct the Directions API request URL
-#     url = f'https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&key={api_key}'
-
-#     # Fetch data from the Directions API
-#     response = requests.get(url)
-#     data = response.json()
-
-#     # Extract distance and duration from the response
-#     if data['status'] == 'OK':
-#         distance = data['routes'][0]['legs'][0]['distance']['text']
-#         duration = data['routes'][0]['legs'][0]['duration']['text']
-#     else:
-#         distance = 'N/A'
-#         duration = 'N/A'
-
-#     return render_template('index.html', distance=distance, duration=duration)
-
-@app.route('/calculate_route', methods=['POST'])
-def calculate_route():
-    origin = request.form['origin']
-    destination = request.form['destination']
-    api_key = 'YOUR_GOOGLE_MAPS_API_KEY'
-
-    # Construct the Directions API request URL
-    url = f'https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&key={api_key}'
-
-    # Fetch data from the Directions API
-    response = requests.get(url)
-    data = response.json()
-
-    # Extract route details from the response
-    if data['status'] == 'OK':
-        route = data['routes'][0]['overview_polyline']['points']
-        distance = data['routes'][0]['legs'][0]['distance']['text']
-        duration = data['routes'][0]['legs'][0]['duration']['text']
-    else:
-        route = None
-        distance = 'N/A'
-        duration = 'N/A'
-
-    return jsonify({'route': route, 'distance': distance, 'duration': duration})
-
-# @app.route('/get-real-time-data')
-# def get_real_time_data():
-#     # Simulated real-time data
-#     time_to_arrival = random.randint(1, 60)  # Random time in minutes
-#     speed = random.randint(20, 60)  # Random speed in km/h
-#     next_stop_distance = random.randint(1, 10)  # Random distance in km
-
-#     return {
-#         'timeToArrival': f'{time_to_arrival} minutes',
-#         'speed': f'{speed} km/h',
-#         'nextStopDistance': f'{next_stop_distance} km'
-#     }
+    print(MAP_API_KEY)
+    return render_template('index.html',map_api_key=MAP_API_KEY )
 
